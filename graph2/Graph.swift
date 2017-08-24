17a89024013971aa2788da7ca9b077e2882d79bf
坂本のぞみ <g15938sn@gm.tsuda.ac.jp>
2017-08-24 10:30:27 +0900

    グラフ描くよ

---
+++ graph2/Graph.swift
@@ -0,0 +1,21 @@
+//
+//  Graph.swift
+//  graph2
+//
+//  Created by 坂本のぞみ on 2017/08/24.
+//  Copyright © 2017年 g15938sn. All rights reserved.
+//
+
+import UIKit
+
+class Graph: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
